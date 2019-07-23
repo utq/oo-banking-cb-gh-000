b7473b0447ec50e0f14e2b7e2402e3bfe800f198
Celal Cankaya <celalcankaya@outlook.com>
2019-07-23 20:59:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
     elsif @status == "complete"
       puts "Transaction was already excuted"
     else
-      @sender.deposit( @amount * -1 ) 
+      @sender.deposit( @amount * -1 )
       @receiver.deposit( @amount )
       @status = "complete"
     end
@@ -32,7 +32,7 @@ class Transfer
 
   def reverse_transfer
     if @status == "complete"
-      @sender.deposit( @amount ) 
+      @sender.deposit( @amount )
       @receiver.deposit( @amount * -1)
       @status = "reversed"
     end
